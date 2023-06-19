f2f43a0f7b15e6a47a13ee3119203409e2b2f6a2
misha <kmsperm97@mail.ru>
2023-06-19 09:55:21 +0300

    Elimination of comments.

--- book_storage/src/main/java/ru/otus/book_storage/actuators/CheckDbHealthIndicator.java
+++ book_storage/src/main/java/ru/otus/book_storage/actuators/CheckDbHealthIndicator.java
@@ -1,6 +1,7 @@
 package ru.otus.book_storage.actuators;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
@@ -11,31 +12,34 @@ import java.sql.SQLException;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CheckDbHealthIndicator implements HealthIndicator {
 
     private final DataSource dataSource;
 
     @Override
     public Health health() {
+        Health health = null;
         try (Connection connection = dataSource.getConnection()) {
             if (connection == null || connection.isClosed()) {
-                return Health
+                health = Health
                         .down()
                         .withDetail("message", "There is no connection to the database")
                         .build();
             }
             if (connection.isValid(5)) {
-                return Health
+                health = Health
                         .up()
                         .withDetail("message", "Connected")
                         .build();
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            log.error(e.getMessage());
+            health = Health
+                    .down()
+                    .withDetail("message", "Failed to get a connection to the database")
+                    .build();
         }
-        return Health
-                .down()
-                .withDetail("message", "Failed to get a connection to the database")
-                .build();
+        return health;
     }
 }
